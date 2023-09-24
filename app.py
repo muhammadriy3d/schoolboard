6d1f01c81f604d9098b16bd210b58f8fae2cac84
muhammadriy3d <mohmmeddasouqi@gmail.com>
2023-09-24 23:25:57 +0300

    delete .vscode from root dir

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ KEY = os.getenv("SECRET_KEY")
 app = create_app(KEY)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True, load_dotenv=True)
+    app.run(host="0.0.0.0{}", port=3000, debug=True, load_dotenv=True)
